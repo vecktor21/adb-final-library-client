c56fe864b0d66da99540bb970640a2e2a6f3b509
Shalkar Koshenayev <shalkar.koshenayev@gmail.com>
2023-11-14 13:02:46 +0600

    select autofill

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import fetchBooks from "@/src/services/fetchBooks";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Home() {
-  const { register, handleSubmit, reset, control } = useForm();
+  const { register, handleSubmit } = useForm();
   const [filter, setFilter] = useState("All");
+  const { data: books, isLoading } = useQuery(["books"], fetchBooks);
+  const [genres, setGenres] = useState([]);
 
-  const userId = localStorage.getItem("userId");
+  useEffect(() => {
+    if (!isLoading && books) {
+      const genresArr = [...new Set(books.map((book) => book.genre))];
+      setGenres(genresArr);
+    }
+  }, [isLoading, books]);
 
-  const results = useQuery(["books"], fetchBooks);
-
-  if (results.isLoading) {
-    return (
-      <div>
-        <h1 style={{ color: "white" }}>Loading</h1>
-      </div>
-    );
-  }
-
-  console.log(results.data);
-
-  const addToCart = async (bookId: string) => {
+  const addToCart = async (bookId) => {
+    const userId = localStorage.getItem("userId");
     const response = await fetch(
       `http://localhost:5000/api/Carts/${userId}/book/${bookId}`,
       {
@@ -54,59 +50,46 @@ export default function Home() {
         })}
       >
         <select className="select" {...register("filter")}>
-          <option value="All">All</option>
-          <option value="Научная литература">Научная литература</option>
-          <option value="Фантастика">Фантастика</option>
-          <option value="Бизнес">Бизнес</option>
+          {genres.map((item, index) => (
+            <option value={item} key={index}>
+              {item}
+            </option>
+          ))}
         </select>
         <input className="apply" type="submit" value="Apply" />
       </form>
       <br />
       <div className="catalog__books">
-        {results.data.map((book, index: number) => {
-          if (filter === "All") {
-            return (
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-                key={index}
-              >
-                <div className="catalog__book pointer">
-                  <Link className="link" href={`/${book.id}`}>
-                    <p className="catalog__book-title">{book.title}</p>
-                    <p className="catalog__book-author">{book.author}</p>
-                    <p className="catalog__book-price">{book.price}</p>
-                  </Link>
+        {!isLoading &&
+          books.map((book, index) => {
+            if (filter === "All" || book.genre === filter) {
+              return (
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                  <div className="catalog__book pointer" key={index}>
+                    <Link className="link" href={`/${book.id}`}>
+                      <p className="catalog__book-title">{book.title}</p>
+                      <p className="catalog__book-author">{book.author}</p>
+                      <p className="catalog__book-price">{book.price}</p>
+                    </Link>
+                  </div>
+                  <input
+                    type="button"
+                    style={{
+                      background: "white",
+                      border: "1px solid black",
+                      padding: "7px 10px",
+                      borderRadius: 15,
+                      marginRight: 50,
+                      marginTop: 20,
+                    }}
+                    value="Add to cart"
+                    onClick={() => addToCart(book.id)}
+                  />
                 </div>
-                <input
-					 className="catalog__book-button pointer"
-					 style={{marginRight: 50, marginTop: 20}}
-                  type="button"
-                  value="Add to cart"
-                  onClick={() => addToCart(book.id)}
-                />
-              </div>
-            );
-          } else if (book.genre === filter) {
-            return (
-              <div className="catalog__book pointer" key={index}>
-                <Link className="link" href={`/${book.id}`}>
-                  <p className="catalog__book-title">{book.title}</p>
-                  <p className="catalog__book-author">{book.author}</p>
-                  <p className="catalog__book-price">{book.price}</p>
-                </Link>
-                <input
-                  type="button"
-                  value="Add to cart"
-                  onClick={() => addToCart(book.id)}
-                />
-              </div>
-            );
-          }
-        })}
+              );
+            }
+            return null;
+          })}
       </div>
     </div>
   );
